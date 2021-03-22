00ddc63fdc5692e7408959f55de19a5544634d31
Hguillenalvarado <heinerguillen@gmail.com>
2021-03-22 06:27:36 +0000

    hasta 40min de taller

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,6 +3,30 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+//ejemplo de funcion formato antiguo
+////function (a,b){
+//    return a+b
+//}
+
+// la funcion anterior en formato moderno: funcion flecha
+
+//const suma=(a,b)=>{
+// return a+b
+//}
+
+// crear un componente min31
+const Container = props => {
+	//parámetros deben ser objetos o sea props
+	// retorna una funcion.  Como ejemplo la de linea 28. debe estar en un div
+	return (
+		<div>
+			<Home />
+			<Home />
+			<Home />
+		</div>
+	);
+};
+
 //create your first component
 export function Home() {
 	return (
